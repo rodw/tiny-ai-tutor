557f9514459297b4db845965eb2d5521052b0096
Rod Waldhoff <rwaldhoff@gmail.com>
2025-10-08 03:04:40 -0400

    convert App.css to App.module.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,18 @@ import ChatMessages from "./components/ChatMessages";
 import ChatInput from "./components/ChatInput";
 import useChat from "./hooks/useChat";
 
-import "./styles/style.css";
-import "./App.css";
-import "./styles/print.css";
+import "./styles/globals.css";
+import styles from "./App.module.css";
 
 export default function App() {
   const { messages, sendMessage, isStreaming, stopStreaming } = useChat();
 
   return (
-    <div className="app-cont">
+    <div className={styles["app-cont"]}>
 
-      <div className="site-head">
+      <div className={styles["site-head"]}>
         <h1>Tiny AI Tutor</h1>
-        <img className="logo-img" src="/logo-192.png" alt="Tiny AI Tutor"/>
+        <img className={styles["logo-img"]} src="/logo-192.png" alt="Tiny AI Tutor"/>
       </div>
 
       <ChatMessages
